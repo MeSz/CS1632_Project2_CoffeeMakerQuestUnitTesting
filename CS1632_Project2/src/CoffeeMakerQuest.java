ef265397e28c5bfee079e995c1e54c908cf91ae8
Joe Meszar <mesz@outk.st>
2016-02-06 13:39:46 -0500

    Finished the Room class & updated requirements
    The room class is finished and ready to be used. The requirements have
    been restructured and the Game and Player classes can now be fleshed
    out.

--- CS1632_Project2/src/CoffeeMakerQuest.java
+++ CS1632_Project2/src/CoffeeMakerQuest.java
@@ -1,73 +1,56 @@
+
 /**
- *
- * @author Joe Meszar (jwm54@pitt.edu) / David Sweeney (dms163@pitt.edu)
+ * CS1632 Project 2: Unit Testing (Coffee Maker Quest)
  * 
- * CS1632 Project 2: Unit Testing Coffee Maker Quest
- * 
- * [REQUIREMENTS]
+ * @author Joe Meszar (jwm54@pitt.edu) | David Sweeney (dms163@pitt.edu)
  *
- *	FUN-ITERATION - At each iteration of the game, the user shall be able enter one of six commands - 
- *          "N" to go North, "S" to go South, "L" to Look for items, "I" for Inventory, 
+ *
+ * --[ REQUIREMENTS ]------------------------------------------------------------------------------------------------
+ *
+ *	[ ] FUN-ITERATION - At each iteration of the game, the user shall be able enter one of six commands -
+ *          "N" to go North, "S" to go South, "L" to Look for items, "I" for Inventory,
  *          "H" for Help, or "D" to Drink.
  *
- *	FUN-UNKNOWN-COMMAND - If a player enters a command not specified by FUN-ITERATION, the system shall 
+ *	[ ] FUN-UNKNOWN-COMMAND - If a player enters a command not specified by FUN-ITERATION, the system shall
  *          respond with the phrase "What?".
  *
- *	FUN-INPUT-CAPS - The system shall be case-insensitive in regards to input values; that is, it shall 
+ *	[ ] FUN-INPUT-CAPS - The system shall be case-insensitive in regards to input values; that is, it shall
  *          accept capital and lower-case letters and treat them as equivalent.
  *
- *	FUN-MOVE - The system shall allow a player to move North only if a door exists going North, and 
+ *	[ ] FUN-MOVE - The system shall allow a player to move North only if a door exists going North, and
  *          South only if a door exists going South.
  *
- *	FUN-WIN - The player shall win the game if and only if Coffee, Sugar, and Cream have been collected 
+ *	[ ] FUN-WIN - The player shall win the game if and only if Coffee, Sugar, and Cream have been collected
  *          by the player and then drunk.
  *
- *	FUN-LOSE - The player shall lose the game if and only if the player Drinks but has not collected all 
+ *	[ ] FUN-LOSE - The player shall lose the game if and only if the player Drinks but has not collected all
  *          of the items (Coffee, Sugar, and Cream).
  *
- *	FUN-INVENTORY - Upon entering "I" for inventory, the player shall be informed of the items that 
+ *	[ ] FUN-INVENTORY - Upon entering "I" for inventory, the player shall be informed of the items that
  *          he/she has collected (consisting of Coffee, Sugar, and Cream).
  *
- *	FUN-LOOK - Upon entering "L" for Look, the player shall collect any items in the room and those items 
+ *	[ ] FUN-LOOK - Upon entering "L" for Look, the player shall collect any items in the room and those items
  *          will be added to the player's inventory.
  *
- *	FUN-HELP - Upon entering "H" for Help, the player shall be shown a listing of possible commands and 
+ *	[ ] FUN-HELP - Upon entering "H" for Help, the player shall be shown a listing of possible commands and
  *          what their effects are.
  *
- *	FUN-UNIQ-ROOM - Each room in the house shall have a unique adjective describing it.
+ *	[ ] FUN-UNIQ-ROOM - Each room in the house shall have a unique adjective describing it.
  *
- *	FUN-UNIQ-ROOM-FURNISHING - Each room in the house shall have one and only one unique furnishing 
+ *	[ ] FUN-UNIQ-ROOM-FURNISHING - Each room in the house shall have one and only one unique furnishing
  *          visible to the user upon entering the room.
  * 
- * What's in the original coffee maker quest?
+ * --------------------------------------------------------------------------------------------[ END REQUIREMENTS ]--
+ *
  * 
- * Player Object
- *      [ ] Inventory
- *          [ ] Coffee
- *          [ ] Cream
- *          [ ] Sugar
- *      [ ] Actions
- *          [ ] Drink (test the inventory for all objects)
- *          [ ] Show Inventory (Print out what the user has/doesn't have)
- *          [ ] Get Coffee (add coffee to player inventory)
- *          [ ] Get Cream (add cream to player inventory)
- *          [ ] Get Sugar (add sugar to player inventory)
- *          [ ] Check Coffee (does player have coffee in inventory)
- *          [ ] Check Cream (does player have cream in inventory)
- *          [ ] Check Sugar (does player have sugar in inventory)
+ * --[ SELF-CHECKS ]-------------------------------------------------------------------------------------------------
  * 
- * Room Object
- *      [ ] Description
- *          [ ] ONE (1) Unique Adjective describing the room
- *              [ ] (this should just be picked from an "adjectives" array)
- *          [ ] ONE (1) Unique Furnishing in the room
- *              [ ] (this should just be picked from a "furnishings" array)
- *          [ ] ONE (1) Unique item in the room
- *              [ ] (this should just be picked from an "items" array)
- * 
- * Game Object
+ * [ ] Game Object
  *      [ ] ONE player object variable
  *      [ ] An array variable of Room objects
+ *      [ ] A unique array of "adjectives"
+ *      [ ] A unique array of "furnishings"
+ *      [ ] An array of "coffee-items". (Coffee, Cream, Sugar)
  *      [ ] Current room number variable (int)
  *      [ ] Input handler
  *          [ ] Case-Insensitive handler for input
@@ -83,7 +66,42 @@
  *              [ ] Only allow this if there exists a "next" room
  *          [ ] "S" go 'South' to previous room. (current room - 1)
  *              [ ] Only allow this if there exists a "previous" room
+ *      [ ] Methods
+ *          [ ] A way to pick a unique adjective from an array so that it is
+ *              never picked again after that.
+ *          [ ] A way to pick a unique furnishing from an array so that it is never picked again.
+ *
+ * [/] Room Object
+ *      [X] ONE (1) Unique Adjective describing the room
+ *          [ ] (this should just be picked from an "adjectives" array)
+ *      [X] ONE (1) Unique Furnishing in the room
+ *          [ ] (this should just be picked from a "furnishings" array)
+ *      [X] ZERO (0) or ONE (1) Unique coffee-related item in the room
+ *          [ ] (this should just be picked from a "coffee-items" array)
+ *
+ * [ ] Player Object
+ *      [ ] Inventory
+ *          [ ] Coffee
+ *          [ ] Cream
+ *          [ ] Sugar
+ *      [ ] Actions
+ *          [ ] Drink (test the inventory for all objects)
+ *          [ ] Show Inventory (Print out what the user has/doesn't have)
+ *          [ ] Get Coffee (add coffee to player inventory)
+ *          [ ] Get Cream (add cream to player inventory)
+ *          [ ] Get Sugar (add sugar to player inventory)
+ *          [ ] Check Coffee (does player have coffee in inventory)
+ *          [ ] Check Cream (does player have cream in inventory)
+ *          [ ] Check Sugar (does player have sugar in inventory)
+ * 
+ * ---------------------------------------------------------------------------------------------[ END SELF-CHECKS ]--
  */
 public class CoffeeMakerQuest {
-    
+
+    public static void main(String args[]) {
+        Room testroom = new Room("Small", "Quaint", "sofa", "Magenta", null, false, true, false);
+
+        System.out.println(testroom);
+    }
+
 }
--- CS1632_Project2/src/Room.java
+++ CS1632_Project2/src/Room.java
@@ -1,60 +1,162 @@
+
 /**
- * [X] Description
- *      [X] ONE (1) Unique Adjective describing the room
- *      [X] ONE (1) Unique Furnishing in the room
- *      [ ] ONE (1) unique item in the room
+ *  [X] ONE Unique adjective describing the room
+ *  [X] ONE Unique Furnishing in the room
+ *  [X] ONE Unique adjective describing the Furnishing in the room
+ *  [X] ZERO or ONE coffee-related items in the room
+ *  [X] ZERO or ONE doors leading North
+ *  [X] ZERO or ONE doors leading South
  */
 public class Room {
+
     private String furnishing = null;       // furnishing in the room
     private String adjFurnishing = null;    // adjective for the furnishing in the room
     private String adjRoom = null;          // adjective for the room
     private String adjNorthDoor = null;     // adjective for the north door in the room
     private String adjSouthDoor = null;     // adjective for the south door in the room
-    
+
     private boolean hasCoffee = false;
     private boolean hasCream = false;
     private boolean hasSugar = false;
-    
-    public Room(String room_adjective, String furnishing_adjective, 
-                String furnishing, String door_north_adjective, String door_south_adjective,
-                boolean hasCoffee, boolean hasCream, boolean hasSugar) {
-        
-        this.adjRoom = room_adjective;
-        this.furnishing = furnishing;
-        this.adjFurnishing = furnishing_adjective;
-        this.adjNorthDoor = door_north_adjective;
-        this.adjSouthDoor = door_south_adjective;
-        
+
+    /**
+     * Creates a new game room, given the following values:
+     * <ul>
+     * <li>A (unique) adjective describing the room.</li>
+     * <li>A (unique) adjective describing the furnishing in the room.</li>
+     * <li>A (unique) furnishing in the room.</li>
+     * <li>A (unique) adjective describing the door leading North (if a door exists).</li>
+     * <li>A (unique) adjective describing the door leading South (if a door exists).</li>
+     * <li>A true/false value indicating whether the 'Coffee' item exists in this room.</li>
+     * <li>A true/false value indicating whether the 'Cream' item exists in this room.</li>
+     * <li>A true/false value indicating whether the 'Sugar' item exists in this room.</li>
+     * </ul>
+     *
+     * @param room_adjective The (unique) adjective describing this room.
+     * @param furnishing_adjective The (unique) adjective describing the furnishing in this room.
+     * @param furnishing The (unique) furnishing in this room.
+     * @param door_north_adjective The (unique) adjective describing the door leading North (<i>null</i> if no door
+     * exists).
+     * @param door_south_adjective The (unique) adjective describing the door leading South (<i>null</i> if no door
+     * exists).
+     * @param hasCoffee True if this room contains the 'Coffee' item; otherwise false.
+     * @param hasCream True if this room contains the 'Cream' item; otherwise false.
+     * @param hasSugar True if this room contains the 'Sugar' item; otherwise false.
+     */
+    public Room(String room_adjective, String furnishing_adjective, String furnishing,
+            String door_north_adjective, String door_south_adjective,
+            boolean hasCoffee, boolean hasCream, boolean hasSugar) {
+
+        if (room_adjective == null) {
+            throw new IllegalArgumentException("The room must contain a (unique) adjective describing it.");
+        } else {
+            this.adjRoom = room_adjective;
+        }
+
+        if (furnishing == null) {
+            throw new IllegalArgumentException("The room must contain a (unique) furnishing.");
+        } else {
+            this.furnishing = furnishing;
+        }
+
+        if (furnishing_adjective == null) {
+            throw new IllegalArgumentException("The room must contain a (unique) adjective describing the furnishing.");
+        } else {
+            this.adjFurnishing = furnishing_adjective;
+        }
+
+        if ((door_north_adjective == null) && (door_south_adjective == null)) {
+            throw new IllegalArgumentException("This room must have at least one door leading North or one door leading South.");
+        } else {
+            this.adjNorthDoor = door_north_adjective;
+            this.adjSouthDoor = door_south_adjective;
+        }
+
         this.hasCoffee = hasCoffee;
         this.hasCream = hasCream;
         this.hasSugar = hasSugar;
     }
-    
+
+    /**
+     * Gets the description of the furnishing in this room.
+     *
+     * @return A description of the unique furnishing in this room.
+     */
     public String getFurnishing() {
-        return String.format("%1%s %2%s\n", this.adjFurnishing, this.furnishing);
+        return String.format("It has a %s %s.\n", this.adjFurnishing, this.furnishing);
     }
-    
+
+    /**
+     * Gets the description of the current room.
+     *
+     * @return A description of the unique furnishing in this room.
+     */
     public String getRoom() {
-        return String.format("You see a %1%s room.\n", this.adjRoom);
+        return String.format("You see a %s room.\n", this.adjRoom);
     }
-    
+
+    /**
+     * Gets a value indicating whether or not this room contains a door leading North.
+     *
+     * @return True if a door exists leading North; otherwise false.
+     */
+    public boolean hasNorthDoor() {
+        return (this.adjNorthDoor != null);
+    }
+
+    /**
+     * Gets a value indicating whether or not this room contains a door leading South.
+     *
+     * @return True if a door exists leading South; otherwise false.
+     */
+    public boolean hasSouthDoor() {
+        return (this.adjSouthDoor != null);
+    }
+
+    /**
+     * Gets the description of the North door in this room (if one exists).
+     *
+     * @return A description of the (unique) door leading North.
+     */
     public String getNorthDoor() {
-        if (this.adjNorthDoor != null) {
-            return String.format("A %1%s door leads North.\n", this.adjNorthDoor);
+        if (this.hasNorthDoor()) {
+            return String.format("A %s door leads North.\n", this.adjNorthDoor);
         }
-        
-        return "";
+
+        return ""; // no North door exists
     }
-    
+
+    /**
+     * Gets the description of the North door in this room (if one exists).
+     *
+     * @return A description of the (unique) door leading North.
+     */
     public String getSouthDoor() {
-        if (this.adjSouthDoor != null) {
-            return String.format("A %1%s door leads South.\n", this.adjSouthDoor);
+        if (this.hasSouthDoor()) {
+            return String.format("A %s door leads South.\n", this.adjSouthDoor);
         }
-        
-        return "";
+
+        return ""; // no South door exists
     }
-    
+
+    /**
+     * Gets a description of the entire room including the type of room, furnishing, and doors leading North or South
+     * (if either one exists).
+     *
+     * @return A description of the entire room.
+     */
     public String getDescription() {
-        return getRoom() + getFurnishing() + getNorthDoor() + getSouthDoor();
+        return "\n" + getRoom() + getFurnishing() + getNorthDoor() + getSouthDoor() + "\n";
+    }
+
+    /**
+     * Gets a description of the entire room including the type of room, furnishing, and doors leading North or South
+     * (if either one exists).
+     *
+     * @return A description of the entire room.
+     */
+    @Override
+    public String toString() {
+        return this.getDescription();
     }
 }
